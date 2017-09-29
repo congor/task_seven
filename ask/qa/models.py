878d33c6b579faa7909750c3f2f700338a19c865
Your Name <gorohovkm@gmail.com>
2017-09-29 21:46:26 +0500

    6th

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,10 +14,10 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(default="", max_length=1024)
     text = models.TextField(default="")
-    added_at = models.DateField(null=True)
+    added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name="q_to_likes")
+    likes = models.ManyToManyField(User, related_name="question__like_user")
 
     def __str__(self):
         return self.title
@@ -28,7 +28,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField(default="")
-    added_at = models.DateField(null=True)
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
